429ff3a0d1cbbecf2c4eaefde00280b48105e58e
Linto <lintojacob2009@gmail.com>
2020-07-18 23:31:07 +0530

    Finished LJAttributedStringF ramework.

---
+++ LJAttributedString/LJAttributedString/LJAttributedString.h
@@ -0,0 +1,19 @@
+//
+//  LJAttributedString.h
+//  LJAttributedString
+//
+//  Created by linto jacob on 18/07/20.
+//  Copyright © 2020 linto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LJAttributedString.
+FOUNDATION_EXPORT double LJAttributedStringVersionNumber;
+
+//! Project version string for LJAttributedString.
+FOUNDATION_EXPORT const unsigned char LJAttributedStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LJAttributedString/PublicHeader.h>
+
+
